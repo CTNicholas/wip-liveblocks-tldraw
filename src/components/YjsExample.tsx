a3046035e9fe944a11401b7f7a1934c7720bb6b0
Chris Nicholas <ctnicholasdev@gmail.com>
2024-08-13 15:17:47 +0100

    feat: Tidy

--- src/components/YjsExample.tsx
+++ src/components/YjsExample.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import "tldraw/tldraw.css";
 import { Tldraw } from "@tldraw/tldraw";
 import { useYjsStore } from "./useYjsStore";
@@ -19,38 +20,7 @@ export function YjsExample() {
       <Tldraw
         autoFocus
         store={store}
-        // See below for why this is commented out
-        // shareZone={<NameEditor />}
       />
     </div>
   );
 }
-
-// From the Liveblocks original fork. Commenting out for now because shareZone is not longer a Tldraw prop
-// const NameEditor = track(() => {
-//   const editor = useEditor();
-
-//   const { color, name } = editor.user;
-
-//   return (
-//     <div style={{ pointerEvents: "all", display: "flex" }}>
-//       <input
-//         type="color"
-//         value={color}
-//         onChange={(e) => {
-//           editor.user.updateUserPreferences({
-//             color: e.currentTarget.value,
-//           });
-//         }}
-//       />
-//       <input
-//         value={name}
-//         onChange={(e) => {
-//           editor.user.updateUserPreferences({
-//             name: e.currentTarget.value,
-//           });
-//         }}
-//       />
-//     </div>
-//   );
-// });
